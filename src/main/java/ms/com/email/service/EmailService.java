bcc8ad1324ad55abda53a99a6cc267f573ed72eb
vtjaeger <viniciustjaeger@gmail.com>
2023-12-04 20:33:39 -0300

    sending content from an Excel table, but in an ugly way

--- src/main/java/ms/com/email/service/EmailService.java
+++ src/main/java/ms/com/email/service/EmailService.java
@@ -1,22 +1,42 @@
 package ms.com.email.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
-    public void enviarEmailSimples(String destinatario, String assunto, String corpo) {
-        SimpleMailMessage mensagem = new SimpleMailMessage();
-        mensagem.setFrom();
-        mensagem.setTo(destinatario);
-        mensagem.setSubject(assunto);
-        mensagem.setText(corpo);
+    LerConteudoCelula leitor = new LerConteudoCelula();
+    public void enviarEmail(String destinatario, String assunto, String corpo, String caminhoAnexo) {
+        MimeMessage mensagem = javaMailSender.createMimeMessage();
 
-        javaMailSender.send(mensagem);
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mensagem, true);
+            helper.setFrom("viniciustoldojaeger@outlook.com");
+            helper.setTo(destinatario);
+            helper.setSubject(assunto);
+
+            String conteudoTabela = leitor.lerConteudoTabela("C:\\Users\\Pichau\\Desktop\\teste.xlsx");
+
+            String corpoComTabela = corpo + "\n\nConteúdo da Tabela:\n\n" + conteudoTabela;
+            helper.setText(corpoComTabela);
+
+            javaMailSender.send(mensagem);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao enviar o e-mail: " + e.getMessage());
+        }
     }
 }
+
+
+
